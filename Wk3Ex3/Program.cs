7c3871fdebc3b54f7cf5a6d6593357e1f43c193b
Jasmine Ramirez-Miller <jassymill@yahoo.com>
2025-02-03 00:42:04 -0600

    update

--- Wk3Ex3/Program.cs
+++ Wk3Ex3/Program.cs
@@ -10,6 +10,27 @@ namespace Wk3Ex3
     {
         static void Main(string[] args)
         {
+            //ask user to enter a string
+            Console.WriteLine("Enter a string: ");
+            string input = Console.ReadLine();
+
+            //counter for vowels 
+            int vowelCount = 0;
+
+            // convert the input to all lowercase 
+            input = input.ToLower();
+
+            //loop to each character to check for vowels
+            foreach (char c in input)
+            {
+                if ("aeiou".Contains(c))
+                {
+                    vowelCount++;
+                }
+            }
+            // Show user total number of vowels 
+            Console.WriteLine("The number of vowels in the string is: " + vowelCount);
+
         }
     }
 }
